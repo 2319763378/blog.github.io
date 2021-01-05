3d4873439ded7f50fa699b8b5fe91f738108f8a4
yqiie <2319763378@qq.com>
2021-01-05 15:51:04 +0800

    Site updated: 2021-01-05 15:50:53

---
+++ dist/music.js
@@ -0,0 +1,92 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    audio: [
+      {
+        name: "洋葱",
+        artist: '杨宗纬',
+        url: 'http://www.ytmp3.cn/down/59662.mp3',
+        cover: 'https://hyiqie.oss-cn-beijing.aliyuncs.com/cto/tajidewoma.png',
+      },
+      {
+        name: '年少有为',
+        artist: '李荣浩',
+        url: 'http://www.ytmp3.cn/down/50491.mp3',
+        cover: 'https://hyiqie.oss-cn-beijing.aliyuncs.com/cto/tajidewoma.png',
+      },
+      {
+        name: '可不可以',
+        artist: '张紫豪',
+        url: 'http://www.ytmp3.cn/down/53158.mp3',
+        cover: 'https://hyiqie.oss-cn-beijing.aliyuncs.com/cto/tajidewoma.png',
+      },
+      {
+        name: '让我留在你身边',
+        artist: '唐汉霄',
+        url: 'http://www.ytmp3.cn/down/54106.mp3',
+        cover: 'https://hyiqie.oss-cn-beijing.aliyuncs.com/cto/tajidewoma.png',
+      },
+      {
+        name: '刚好遇见你',
+        artist: '李玉刚',
+        url: 'http://www.ytmp3.cn/down/38579.mp3',
+        cover: 'https://hyiqie.oss-cn-beijing.aliyuncs.com/cto/tajidewoma.png',
+      },
+      {
+        name: '后来',
+        artist: '刘若英',
+        url: 'http://www.ytmp3.cn/down/48101.mp3',
+        cover: 'https://hyiqie.oss-cn-beijing.aliyuncs.com/cto/tajidewoma.png',
+      },
+      {
+        name: '那些年',
+        artist: '胡夏',
+        url: 'http://www.ytmp3.cn/down/49529.mp3',
+        cover: 'https://hyiqie.oss-cn-beijing.aliyuncs.com/cto/tajidewoma.png',
+      },
+      {
+        name: '像我这样的人',
+        artist: '毛不易',
+        url: 'http://www.ytmp3.cn/down/50667.mp3',
+        cover: 'https://hyiqie.oss-cn-beijing.aliyuncs.com/cto/tajidewoma.png',
+      },
+      {
+        name: '水星记',
+        artist: '郭顶',
+        url: 'http://www.ytmp3.cn/down/55401.mp3',
+        cover: 'https://hyiqie.oss-cn-beijing.aliyuncs.com/cto/tajidewoma.png',
+      },
+      {
+        name: '云烟成雨',
+        artist: '房东的猫',
+        url: 'http://www.ytmp3.cn/down/48951.mp3',
+        cover: 'https://hyiqie.oss-cn-beijing.aliyuncs.com/cto/tajidewoma.png',
+      },
+      {
+        name: '其实都没有',
+        artist: '杨宗纬',
+        url: 'http://www.ytmp3.cn/down/59429.mp3',
+        cover: 'https://hyiqie.oss-cn-beijing.aliyuncs.com/cto/tajidewoma.png',
+      },
+      {
+        name: '一个人想着一个人',
+        artist: '曾沛慈',
+        url: 'http://www.ytmp3.cn/down/56117.mp3',
+        cover: 'https://hyiqie.oss-cn-beijing.aliyuncs.com/cto/tajidewoma.png',
+      },
+      {
+        name: '他和她',
+        artist: '回音哥',
+        url: 'http://www.ytmp3.cn/down/60168.mp3',
+        cover: 'https://hyiqie.oss-cn-beijing.aliyuncs.com/cto/tajidewoma.png',
+      },
+      {
+        name: '纸短情长',
+        artist: '阿冷',
+        url: 'http://www.ytmp3.cn/down/49012.mp3',
+        cover: 'https://hyiqie.oss-cn-beijing.aliyuncs.com/cto/tajidewoma.png',
+      }
+
+
+    ]
+});
